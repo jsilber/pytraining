9db544749b19629092544c16fcd9c00f4b2f3ed8
Jackie Silber <jsilbertech@gmail.com>
2018-03-25 16:58:41 -0700

    First program

---
+++ program.py
@@ -0,0 +1,15 @@
+#JSilber March 25, 2018
+#App 1 in Jumpstart by Building 10 Apps
+
+#Create simple header to display in console window
+
+#Example of string literal. Assigning string in place.
+print ('---------------------------------')
+print ('            HELLO APP            ')
+print ('---------------------------------')
+
+#takes an input from user at console
+#In Python, use underscore to name variables. #Not camel casing
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_text
+print(greeting)
